b323b0a5e645ee4c5cc5015d88d36aa431708b74
erfuuan <erfuuan@gmail.com>
2025-03-02 13:43:59 +0330

    Deploy website - based on 8554743e9cfc1c9f436672d6b5093c8f27a2c310

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/erfuuan/selfProject/NODEJS/erfuuan.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/erfuuan/selfProject/NODEJS/erfuuan.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/erfuuan/selfProject/NODEJS/erfuuan.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/erfuuan/selfProject/NODEJS/erfuuan.github.io/src/css/custom.css"),
-];
